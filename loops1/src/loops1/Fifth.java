159bc4caecdfc1455d544115b2bea2e04cfac227
vishnuvvss <117639963+vishnuvvss@users.noreply.github.com>
2024-09-13 23:16:10 +0530

    Fifth.java

--- loops1/src/loops1/Fifth.java
+++ loops1/src/loops1/Fifth.java
@@ -1,10 +1,12 @@
 package loops1;
-
+import java.util.Scanner;
 public class Fifth {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=65,sum=0,i=1;
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int sum=0,i=1;
 	
 		while(i<=num/2) {	
 			
