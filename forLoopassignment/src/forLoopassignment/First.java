5ee86a0eb7b83f14d5c2a134b203f7221c113a5d
vishnuvvss <117639963+vishnuvvss@users.noreply.github.com>
2024-09-15 23:25:48 +0530

    OddSum.java

--- forLoopassignment/src/forLoopassignment/First.java
+++ forLoopassignment/src/forLoopassignment/First.java
@@ -1,13 +1,15 @@
 package forLoopassignment;
-
-public class First {
+import java.util.Scanner;
+public class OddSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=6,sum=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
 			for(int i=1;i<=n;i++) {
 				if(i%2!=0) {
-					sum=sum+i;
+				    sum=sum+i;
 				}
 			}
 			System.out.println(sum);
