ea6b0777865eea4d0f0f1929ce30be4706a2befb
vishnuvvss <117639963+vishnuvvss@users.noreply.github.com>
2024-09-18 23:03:54 +0530

    Update Seventh.java

--- forLoopassignment/src/forLoopassignment/Seventh.java
+++ forLoopassignment/src/forLoopassignment/Seventh.java
@@ -1,15 +1,15 @@
 package forLoopassignment;
-
+import java.util.Scanner;
 public class Seventh {
 	public static void main(String[] args) {
-		int div=5;
-		
+		Scanner sc=new Scanner(System.in);
+		int div=sc.nextInt();
 		
 		for(int i=5;i<=100;i++)
 		{
-			if(i%div==0) {
-			System.out.println(i+" is divisible to 5");
-		}
-	}
+		   if(i%div==0) {
+		        System.out.println(i+" is divisible to 5");
+		  }
+	       }
 	}
 }
