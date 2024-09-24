d5ae9c77f4b382d175480736c7101856a9b299b5
vishnuvvss <117639963+vishnuvvss@users.noreply.github.com>
2024-09-24 12:48:18 +0530

    Fibanocci_series.java

--- loops1/src/loops1/Third.java
+++ loops1/src/loops1/Third.java
@@ -1,15 +1,18 @@
 package loops1;
-
-public class Third {
+import java.util.Scanner;
+public class Fibanocci_series {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=0,b=1,n=10;
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt(); // 1st tern in series
+		int b=sc.nextInt(); // 2nd term in series
+		int n=sc.nextInt(); // for series range upto 'n' th  term
 		System.out.println(a);//0
 		System.out.println(b);//1
 		while(n--!=0) {
 		    
-			int	c=a+b;//1,2,3,5,8
+			int c=a+b;//  1,2,3,5,8
 			a=b;//
 			b=c;//swapping and adding for n fibannoci series
 			System.out.println(c);
