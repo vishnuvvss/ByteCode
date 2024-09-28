c5f96b31aa0b3220e93ff049dd06cb0288b48cc8
vishnuvvss <117639963+vishnuvvss@users.noreply.github.com>
2024-09-28 22:30:27 +0530

    Update Ninth.java

--- forLoopassignment/src/forLoopassignment/Ninth.java
+++ forLoopassignment/src/forLoopassignment/Ninth.java
@@ -1,8 +1,10 @@
 package forLoopassignment;
-
+import java.util.Scanner;
 public class Ninth {
 	public static void main(String[] args ) {
-		int n=10, square=1;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		square=1; //  to square n numbers
 		for(int i=1;i<=n;i++) {
 			square=i*i;
 			System.out.println("The square of "+i+":"+square);
