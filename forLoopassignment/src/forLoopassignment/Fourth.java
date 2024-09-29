ce174d5b83f43916e2ac2535c7c0eae68b1eb1f5
vishnuvvss <117639963+vishnuvvss@users.noreply.github.com>
2024-09-29 19:14:10 +0530

    Update Fourth.java

--- forLoopassignment/src/forLoopassignment/Fourth.java
+++ forLoopassignment/src/forLoopassignment/Fourth.java
@@ -1,10 +1,13 @@
 package forLoopassignment;
-
+import java.util.Scanner;
 public class Fourth {
 	public static void main(String[] args) {
-		for(int m=120;m<=130;m++) {
+		Scanner sc=new Scanner(System.in);
+		int m=0;	// starting number
+		int n=sc.nextInt(); //ended number
+		for(m=sc.nextInt();m<=n;m++) {
 			if(m%2==0)
-			 System.out.println(m);
+			 System.out.println(m); // printing m to n with divisble by 2 or even number between m and n
 		}
 	}
 }
